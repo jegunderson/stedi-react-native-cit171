5147a27ebbcd951c416f5f59214bccd933ffd4e9
jegunderson <jegunderson@byui.edu>
2022-06-10 10:14:26 -0600

    tabs

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import {useState} from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Counter from './Counter.js';
 import SettingsScreen from './SettingsScreen.js';
+import Login from './Login.js';
 import Home from './Home.js';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -13,6 +14,10 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  
+  if(userLoggedIn){
+
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -54,6 +59,13 @@ export default function App() {
     </NavigationContainer>
   );
 
+  } else{
+    return (
+      <View>
+       <Login/> 
+      </View>
+    )
+  }
 
 }
 
